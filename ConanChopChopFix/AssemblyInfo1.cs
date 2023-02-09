b0516b6b0667cfe43f11b4075cb245f967e25e2f
Trevor <trevorguidry6@gmail.com>
2023-02-08 19:35:53 -0600

    Add project files.

---
+++ ConanChopChopFix/AssemblyInfo1.cs
@@ -0,0 +1,5 @@
+using ConanChopChopFix;
+using MelonLoader;
+// ...
+[assembly: MelonInfo(typeof(Class1), "ConanChopChopFix", "1.0.0", "Trevor159")]//MelonInfo(typeof(ClassOfMod), "Game Mod Name", "Version", "Author")
+[assembly: MelonGame("", "Conan Chop Chop")] //MelonGame("Studio", "Game")]
---
+++ ConanChopChopFix/Class1.cs
@@ -0,0 +1,46 @@
+﻿using MelonLoader;
+using HarmonyLib;
+using Il2Cpp;
+
+namespace ConanChopChopFix
+{
+    public class Class1 : MelonMod
+    {
+        [HarmonyPatch(typeof(DirectionSignPanel), "Update")]
+        static class Patch
+        {
+            static void Postfix(DirectionSignPanel __instance)
+            {
+                try
+                {
+
+                    UnityEngine.GameObject directionPanel = __instance.gameObject;
+                    UnityEngine.GameObject parent = directionPanel.transform.GetChild(0).gameObject;
+                    UnityEngine.GameObject continueButton = parent.transform.GetChild(5).gameObject;
+                    UnityEngine.GameObject waitingForPlayers = parent.transform.GetChild(6).gameObject;
+                    continueButton.SetActive(true);
+                    waitingForPlayers.SetActive(false);
+                    __instance.canClose = true;
+
+                    UnityEngine.GameObject buttonText = continueButton.transform.GetChild(0).gameObject;
+
+                    Il2CppTMPro.TMP_Text m_TextComponent = buttonText.GetComponent<Il2CppTMPro.TMP_Text>();
+                    m_TextComponent.text = "Continue*";
+                }
+                catch (System.Exception ex)
+                {
+                    Melon<Class1>.Logger.Warning($"Exception in patch of void DirectionSignPanel::Update():\n{ex}");
+                }
+            }
+        }
+
+
+        /*public override void OnInitializeMelon()
+        {
+            HarmonyPa
+            MethodInfo privateMethod = typeof(DirectionSignPanel).GetMethod("Update");
+
+        }*/
+
+    }
+}
